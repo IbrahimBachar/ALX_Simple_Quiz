621739bab8a9ae4f7927f5d071fd21d46a7db549
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-01-27 00:35:30 +0200

    last updated quiz

--- quiz.js
+++ quiz.js
@@ -1,15 +1,23 @@
 
 function checkAnswer(){
     const correctAnswer = "4";
-    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+    
 
-    if(userAnswer === correctAnswer){
-        const feedback = document.getElementById("feedback");
-        feedback.textContent = "Correct! Well done.";
+    if(selectedAnswer){
+        const userAnswer = selectedAnswer.value;
+        if(userAnswer === correctAnswer){
+            const feedback = document.getElementById("feedback");
+            feedback.textContent = "Correct! Well done.";
+        }else{
+            const feedback = document.getElementById("feedback");
+            feedback.textContent = "That's incorrect. Try again!";
+        }
     }else{
         const feedback = document.getElementById("feedback");
-        feedback.textContent = "That's incorrect. Try again!";
+        feedback.textContent = "Please select an answer.";
     }
+    
 }
 
 const submitButton = document.getElementById("submit-answer");
